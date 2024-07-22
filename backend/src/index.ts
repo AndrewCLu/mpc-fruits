6d3be62227ae91c9fba98f849260ecd2b9a12517
AndrewCLu <andrewclu98@gmail.com>
2024-07-22 18:44:17 -0400

    try ignore

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,8 @@
+// @ts-ignore
 import path from "path";
+// @ts-ignore
 import express from "express";
+// @ts-ignore
 import http from "http";
 // @ts-ignore
 import { JIFFServer, JIFFServerBigNumber } from "jiff-mpc";
@@ -7,6 +10,7 @@ import { JIFFServer, JIFFServerBigNumber } from "jiff-mpc";
 const app = express();
 const server = http.createServer(app);
 
+// @ts-ignore
 app.use("/", express.static(path.join(__dirname)));
 
 const jiffServer = new JIFFServer(server, { logs: true });
