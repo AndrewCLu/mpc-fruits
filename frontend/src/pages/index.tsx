f9e46cd0837f4099726307629edf29a03bfee816
AndrewCLu <andrewclu98@gmail.com>
2024-07-22 19:44:52 -0400

    no log

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -11,7 +11,6 @@ const JiffClientComponent: React.FC = () => {
   const [buttonDisabled, setButtonDisabled] = useState<boolean>(true);
 
   const connect = () => {
-    console.log("Connecting...", process.env.NODE_ENV);
     const client = new JIFFClient(
       process.env.NODE_ENV === "development"
         ? "http://localhost:8080"
