3f1eb5635fdc0b39c07b7afd023184a6bb774314
AndrewCLu <andrewclu98@gmail.com>
2024-07-22 18:49:07 -0400

    remove comments

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,5 @@
-// @ts-ignore
 import path from "path";
-// @ts-ignore
 import express from "express";
-// @ts-ignore
 import http from "http";
 // @ts-ignore
 import { JIFFServer, JIFFServerBigNumber } from "jiff-mpc";
@@ -10,7 +7,6 @@ import { JIFFServer, JIFFServerBigNumber } from "jiff-mpc";
 const app = express();
 const server = http.createServer(app);
 
-// @ts-ignore
 app.use("/", express.static(path.join(__dirname)));
 
 const jiffServer = new JIFFServer(server, { logs: true });
